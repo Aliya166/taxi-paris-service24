01de55bcc7051ecd67f2c3278c3cb24bfe562c36
Alisa Zamkova <alisazamkovaya@gmail.com>
2026-09-06 15:45:10 +0200

    Add homepage loyalty progress

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +14,25 @@ final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
     #[Route('/index.html', name: 'app_home_legacy', methods: ['GET'])]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig');
+    public function index(
+        ReservationRepository $reservationRepository
+    ): Response {
+        $user = $this->getUser();
+
+        $loyaltyProgress = null;
+        $ridesUntilDiscount = null;
+
+        if ($user instanceof User) {
+            $completedRides = $reservationRepository
+                ->countCompletedInCurrentLoyaltyCycleByCustomer($user);
+
+            $loyaltyProgress = min(5, $completedRides);
+            $ridesUntilDiscount = max(0, 5 - $loyaltyProgress);
+        }
+
+        return $this->render('home/index.html.twig', [
+            'loyaltyProgress' => $loyaltyProgress,
+            'ridesUntilDiscount' => $ridesUntilDiscount,
+        ]);
     }
 }
